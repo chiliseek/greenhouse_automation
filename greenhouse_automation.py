1347cf12fdec34043716bf1c003e5af9abd294ef
Jo Zen <acc.joo@gmail.com>
2019-02-11 20:48:58 +0100

    testing relay

--- greenhouse_automation.py
+++ greenhouse_automation.py
@@ -4,11 +4,44 @@ from time import sleep
 import RPi.GPIO as GPIO
 import Adafruit_DHT
 
-print("Testing DHT22")
+print("Testing DHT22...")
 sensor = Adafruit_DHT.DHT22
 pin = 14
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 print(humidity, temperature)
+
+
+print("\nTesting Relay...")
+GPIO.setmode(GPIO.BCM)  # Setting GPIO mode
+
+pinliste = [
+    23,  # Channel 1
+    18,  # Channel 2
+    24,  # Channel 3
+    25,  # Channel 4
+]
+
+print("Initializing relay board...")
+for pin in pinliste:
+    GPIO.setup(pin, GPIO.OUT)
+
+print("Starting Knight Rider in a second...")
+
+counter = 0
+while counter < 1:  # Relay Knight Rider
+    for pin in pinliste:
+        GPIO.output(pin, GPIO.LOW)
+        time.sleep(0.05)
+        GPIO.output(pin, GPIO.HIGH)
+
+    for pin in reversed(pinliste):
+        GPIO.output(pin, GPIO.LOW)
+        time.sleep(0.05)
+        GPIO.output(pin, GPIO.HIGH)
+
+    counter +=1
+
+print("Stopped Knight Rider...")
 
 
 """
