ec3db1b1120f3183851dc35a032cd4e0f6739ccf
Jo Zen <acc.joo@gmail.com>
2019-02-11 20:26:10 +0100

    testing dht22

--- greenhouse_automation.py
+++ greenhouse_automation.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 #  Greenhouse automation with python.
+from time import sleep
+import RPi.GPIO as GPIO
+import Adafruit_DHT
 
+print("Testing DHT22")
+sensor = Adafruit_DHT.DHT22
+pin = 14
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+print(humidity, temperature)
+
+
+"""
+class DHT22():
+    """Access the DHT22 sensor"""
+    
+    def __init__(self):
+"""
